cc544046a8767b9cb800aaa075cbf174343fef86
Johanson Felix <felix.johanson@gmail.com>
2021-10-14 17:31:11 -0400

    complted app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
-import { Swtich, Route, Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
-import './App.css';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import { Layout } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
-      <div className="main"></div>
-      <Navbar />
-      <div className="footer"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
     </div>
   );
 };
